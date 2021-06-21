093322c8eb543565f95a564046395bb7d6f23988
weng-alt <896313711@qq.com>
2021-06-21 13:49:49 +0800

    s

--- springMvc01/src/main/java/com/panshi/controller/UserController.java
+++ springMvc01/src/main/java/com/panshi/controller/UserController.java
@@ -3,30 +3,58 @@ package com.panshi.controller;
 import com.panshi.pojo.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+
     @GetMapping("/hello")
-    public String Hello(){
+    public String Hello() {
         System.out.println("进来了,lll");
         return "hello";
     }
 
     @GetMapping("/test1/{name}")
-    public String test1(@PathVariable String name){
-        System.out.println("test1进来了"+name);
+    public String test1(@PathVariable String name) {
+        System.out.println("test1进来了" + name);
         return "hello";
     }
 
-    @PostMapping("/ajax")
-    public String ajax(@RequestBody Student student){
+    @GetMapping("/test2")
+    public String test2(Student student) {
         System.out.println(student);
         return "hello";
     }
 
+
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public String upload(HttpServletRequest req, MultipartFile file) {
+        String path = req.getServletContext().getRealPath("/uploads/");
+        File f = new File(path);
+        if (!f.exists())
+            f.mkdirs();
+        String fileName = file.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+
+        fileName = uuid + "_" + fileName;
+        System.out.println(path);
+        try {
+            file.transferTo(new File(path,fileName));
+        } catch (IOException e) {
+            System.err.println("上传文件失败");
+        }
+        return "";
+    }
 
 
 }
