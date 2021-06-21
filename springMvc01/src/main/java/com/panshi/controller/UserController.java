56f057fa93d86f2258412a496c8f6e4d9777d1ab
weng-alt <896313711@qq.com>
2021-06-21 10:26:52 +0800

    wss

--- springMvc01/src/main/java/com/panshi/controller/UserController.java
+++ springMvc01/src/main/java/com/panshi/controller/UserController.java
@@ -2,57 +2,33 @@ package com.panshi.controller;
 
 import com.panshi.pojo.Student;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-
     @GetMapping("/hello")
-    public String Hello() {
+    public String Hello(){
         System.out.println("进来了,lll");
         return "hello";
     }
 
     @GetMapping("/test1/{name}")
-    public String test1(@PathVariable String name) {
-        System.out.println("test1进来了" + name);
+    public String test1(@PathVariable String name){
+        System.out.println("test1进来了"+name);
         return "hello";
     }
 
     @GetMapping("/test2")
-    public String test2(Student student) {
+    public String test2(Student student){
         System.out.println(student);
         return "hello";
     }
 
-    @PostMapping("/upload")
-    @ResponseBody
-    public String upload(HttpServletRequest req, MultipartFile file) {
-        String path = req.getServletContext().getRealPath("/uploads/");
-        File f = new File(path);
-        if (!f.exists())
-            f.mkdirs();
-        String fileName = file.getOriginalFilename();
-        String uuid = UUID.randomUUID().toString().replace("-", "");
-
-        fileName = uuid + "_" + fileName;
-        System.out.println(path);
-        try {
-            file.transferTo(new File(path,fileName));
-        } catch (IOException e) {
-            System.err.println("上传文件失败");
-        }
-        return "";
-    }
 
 
 }
--- springMvc01/src/main/webapp/index.jsp
+++ springMvc01/src/main/webapp/index.jsp
@@ -1,9 +1,5 @@
 <html>
 <body>
 <h2>Hello World!</h2>
-<form action="/user/upload" method="post" enctype="multipart/form-data">
-    <input type="file" name="file">
-    <input type="submit" />
-</form>
 </body>
 </html>
